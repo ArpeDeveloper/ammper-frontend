250e65a9c8c5312bffad9bfb29306dbe64c47d7f
Jesus Ross <jesus.ross@outlook.com>
2024-01-12 18:54:40 -0600

    progress on home template

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,74 @@
+"use client"
 
-export default function Home() {
+import * as React from "react" 
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import { Separator } from "@/components/ui/separator"
+import { Payment, columns } from "@/components/transactions/columns"
+import { DataTable } from "@/components/transactions/transactionsTable"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ]
+}
+
+export default async function Home() {
+    const data = await getData()
     return (
-      <main className="grid grid-cols-2 min-h-screen text-black p-8">
-        <div>Uno</div>
-        <div>Uno</div>
+      <main className="grid gap-4 grid-cols-1 md:grid-cols-5 text-black p-8">
+        <div className="px-14 col-span-2" >
+          <Carousel >
+            <CarouselContent>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index}>
+                    <Card>
+                      <CardContent className="flex aspect-[2/1] items-center justify-center p-6">
+                        <span className="text-4xl font-semibold">{index + 1}</span>
+                      </CardContent>
+                    </Card>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+
+          <Separator className="my-4" />
+
+          <DataTable columns={columns} data={data} />
+        </div>
+        
+        <div className="col-span-3 flex">
+          <Separator orientation="vertical" />
+          <Accordion className="px-6 w-full" type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger>Is it accessible?</AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
       </main>
     )
   }
